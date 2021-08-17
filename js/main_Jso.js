a2aa51b92b1646a44037c5b3ad5a9e1e28c104da
hanminho0426 <l9930426@naver.com>
2021-08-17 16:38:06 +0900

    영화검색

---
+++ js/main_Jso.js
@@ -0,0 +1,45 @@
+//JSON (JavaScript Object Notation)
+// 자바스크립트의 객체 표기법
+
+// import myData from '../myData.json'
+
+// console.log(myData)
+
+// const user = {
+//   name: 'git',
+//   age: 85,
+//   email: [
+//     'l9930426@naver.com',
+//     'neo@naver.com'
+//   ]
+// }
+// console.log('user', user)
+
+// const str = JSON.stringify(user)
+// console.log('str', str)
+// console.log(typeof str)
+
+// const obj = JSON.parse(str)
+// console.log('obj', obj)
+
+//----------------------------
+// Storage
+// const user = {
+//   name: 'git',
+//   age: 85,
+//   email: [
+//     'l9930426@naver.com',
+//     'neo@naver.com'
+//   ]
+// }
+
+// // localStorage.setItem('user', JSON.stringify(user))
+// // console.log(JSON.parse(localStorage.getItem('user')))
+// //localStorage.removeItem('user') //제거
+
+// const str = localStorage.getItem('user')
+// const obj = JSON.parse(str)
+// obj.age =22
+// console.log(obj)
+// localStorage.setItem('user', JSON.stringify(obj))
+
---
+++ js/movie.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+//querystring
+//주소?속성=값&속성=값&속성=값
+
+// function fetchMovies(){
+//   axios
+//   .get('https://www.omdbapi.com/?apikey=7035c60c&s=frozen')
+//   .then(res =>{
+//     console.log(res)
+//     const h1El = document.querySelector('h1')
+//     const imgEl = document.querySelector('img')
+//     h1El.textContent = res.data.Search[0].Title
+//     imgEl.src = res.data.Search[0].Poster
+//   }) 
+// }
+// fetchMovies()
